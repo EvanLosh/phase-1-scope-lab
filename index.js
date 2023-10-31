7068fbc1abaaa760ddf518862909c8bdfae58770
Evan Losh <evand.losh@gmail.com>
2023-10-31 09:26:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// setBestCustomer();
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'mary';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not mary';
+}
+
+// changeLeastFavoriteCustomer();
